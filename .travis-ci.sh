4307c9aaf79c2e540c2dd9c27fc926c71a070998
Akinori ABE <abe@kb.ecei.tohoku.ac.jp>
2015-08-19 13:47:59 +0900

    Initial commit

---
+++ .travis-ci.sh
@@ -0,0 +1,28 @@
+set -eu
+
+# OPAM packages you install
+OPAM_DEPS="ocamlfind core lwt ounit"
+
+export PREFIX="./usr"
+export BINDIR="$PREFIX/bin"
+export LIBDIR="$PREFIX/lib"
+export PATH="$BINDIR:$PATH"
+
+mkdir -p $PREFIX
+
+# Download and install OPAM and OCaml
+wget -q -O opam_installer.sh "https://raw.github.com/ocaml/opam/master/shell/opam_installer.sh"
+if [ -n "${OPAM_VERSION:-}" ]; then
+    sed -i "s/^VERSION=.*$/VERSION='$OPAM_VERSION'/" opam_installer.sh
+fi
+echo y | sh opam_installer.sh $BINDIR $OCAML_VERSION
+
+export OPAMYES=1
+export OPAMVERBOSE=1
+opam init
+eval `opam config env`
+
+# Install OPAM packages
+if [ -n "${OPAM_DEPS:-}" ]; then
+    opam install $OPAM_DEPS
+fi
